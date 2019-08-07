f9e6f4585b3b6a005f80cd5ee66b70f28a08ffcb
crist1995 <crzuniga@alumnos.uai.cl>
2019-08-07 11:57:02 -0300

    second commit

--- ripley_api.py
+++ ripley_api.py
@@ -6,17 +6,6 @@ import redis_set
 redis_set.set()
 app = Flask(__name__)
 
-@app.route('/api/data', methods=['GET'])
-def get_tasks1():
-      query2="SELECT *  FROM test.nieve2"
-      mycursor.execute(query2)
-      row_headers=[x[0] for x in mycursor.description]
-      data = mycursor.fetchall()
-      json_data=[]
-      for result in data:
-        json_data.append(dict(zip(row_headers,result)))
-      return jsonify({"data": json_data})
-
 @app.route('/api', methods=['GET'])
 def get_task(task_id):
     #def fucnt
