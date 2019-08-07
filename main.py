54ac956ff161182db943107fd08ac4a80c22c5d2
crist1995 <crzuniga@alumnos.uai.cl>
2019-08-07 12:43:33 -0300

    api updates

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import temp_time as tat
 import redis_set2 as rs
 import main2
 rs.set()
+data =[]
 ciudad=["santiago","zurich"]
 ciudades=["santiago","nueva-york","zurich","auckland", "georgia"]
-for i in ciudades:
- print(i)
- main2.conn(i)
-# pos= rc.getter(i)
-# print(pos[b'longitud'])
-# print(tat.gettat(pos[0],pos[1]))
-# print(i)
+def cycle():
+ for i in ciudades:
+  print(i)
+  main2.conn(i)
+  pos= rc.getter(i)
+  print(pos[b'longitud'])
+  data.append(tat.gettat(pos[b'latitud'],pos[b'longitud']))
+ return data
--- ripley_api.py
+++ ripley_api.py
@@ -2,9 +2,20 @@
 from flask import Flask, jsonify, request
 import redis_caller  as rc
 import redis_set
+from time import sleep
+import main
+from flask_socketio import SocketIO
 
 redis_set.set()
 app = Flask(__name__)
+socketio=SocketIO(app)
+
+@socketio.on('update_info', namespace='/test')
+def test_message(message):
+  while true:
+    time.sleep(10)
+    data=main.cycle()
+    emit({'data', data})
 
 @app.route('/api', methods=['GET'])
 def get_task(task_id):
@@ -16,5 +27,5 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-      app.run(host='0.0.0.0')
+      socketio.run(app,host='0.0.0.0')
 
